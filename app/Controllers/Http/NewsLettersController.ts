72a608d45319280b60688dc14054911761eaf91f
Azarow <contactazarox@gmail.com>
2023-03-18 09:28:26 +0100

    feat: add newsletter store et destroy

--- app/Controllers/Http/NewsLettersController.ts
+++ app/Controllers/Http/NewsLettersController.ts
@@ -1,6 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema } from '@ioc:Adonis/Core/Validator'
 import Email from 'App/Models/Email'
+import Mail from '@ioc:Adonis/Addons/Mail'
 
 export default class NewsLettersController {
   public async store({ request, response }: HttpContextContract) {
@@ -11,6 +12,21 @@ export default class NewsLettersController {
 
     await Email.create(data)
 
+    await Mail.send((message) => {
+      message
+        .from('noreply@notybot.org')
+        .to(data.email)
+        .subject('Sign Up for NotyBot Alpha')
+        .htmlView('emails/welcome', { email: data.email })
+    })
+
     return response.send('Email added !')
+  }
+
+  public async delete({ params, response }: HttpContextContract) {
+    const email = await Email.findBy('email', params.email)
+    await email!.delete()
+
+    return response.send('Email deleted !')
   }
 }
