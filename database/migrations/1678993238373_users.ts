9a0b70a1702a0246a668255c2230f784267add42
Azarow <contactazarox@gmail.com>
2023-03-20 04:05:35 +0100

    feat: change id int to id string

--- database/migrations/1678993238373_users.ts
+++ database/migrations/1678993238373_users.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.string('id').primary()
       table.string('email', 255).notNullable()
       table.string('username').notNullable()
       table.string('provider')
