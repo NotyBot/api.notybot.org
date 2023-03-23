9464c9af59acc561892ae7b76b03db9d58644922
Azarow <contactazarox@gmail.com>
2023-03-23 02:35:35 +0100

    feat: add welcome validator

---
+++ app/Validators/WelcomeValidator.ts
@@ -0,0 +1,20 @@
+import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { EmbedSchema } from 'App/Validators/Constants/WelcomeSchema'
+
+export type WelcomeData = WelcomeValidator['schema']['props']
+export default class WelcomeValidator {
+  constructor(protected ctx: HttpContextContract) {}
+  public schema = schema.create({
+    guild_id: schema.string(),
+    channel_id: schema.string(),
+    message: schema.string.nullable(),
+    canvas: schema.string.nullable(),
+    embed: EmbedSchema,
+    role: schema.string.nullable(),
+    send_private_message: schema.boolean(),
+    give_role: schema.boolean(),
+    enabled: schema.boolean(),
+  })
+  public messages: CustomMessages = {}
+}
