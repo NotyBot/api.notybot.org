5bf945b9692a828f10fb33482a9f029cc75d1000
Azarow <contactazarox@gmail.com>
2023-03-23 02:34:49 +0100

    feat: edit type Welcome

--- app/Validators/Constants/WelcomeSchema.ts
+++ app/Validators/Constants/WelcomeSchema.ts
@@ -1,19 +1,20 @@
 import { schema } from '@ioc:Adonis/Core/Validator'
 
-export const EmbedFieldType = schema.object.optional().members({
+export const EmbedFieldType = schema.object.nullable().members({
   name: schema.string.nullable(),
   value: schema.string.nullable(),
   inline: schema.boolean.optional(),
 })
 
-export const EmbedFooterType = schema.object.optional().members({
+export const EmbedFooterType = schema.object.nullable().members({
   text: schema.string.nullable(),
   iconUrl: schema.string.nullable(),
 })
 
 export const EmbedSchema = schema.object.optional().members({
+  title: schema.string.nullable(),
   description: schema.string(),
   color: schema.string(),
-  fields: schema.array().members(EmbedFieldType),
+  fields: schema.array.nullable().members(EmbedFieldType),
   footer: EmbedFooterType,
 })
