fe55782b81d2b9c4c5666e9ab427bb431f3c657a
Thibault <contactazarox@gmail.com>
2023-04-20 03:46:26 +0200

    fix(bot): split request header Authorization

--- app/Middleware/AuthBot.ts
+++ app/Middleware/AuthBot.ts
@@ -3,13 +3,14 @@ import ApiCredential from 'App/Models/ApiCredential'
 
 export default class AuthBotMiddleware {
   public async handle({ request, response }: HttpContextContract, next: () => Promise<void>) {
-    const botCredential = await ApiCredential.findBy('bearerToken', request.header('Authorization'))
+    const token = request.header('Authorization')!.split(' ')[1]
+    const botCredential = await ApiCredential.findBy('bearer_token', token)
 
     if (!botCredential) {
       return response.status(401).json({ message: 'Unauthorized' })
     }
 
-    if (botCredential.bearer_token !== request.header('Authorization')) {
+    if (botCredential.bearer_token !== token) {
       return response.status(401).json({ message: 'Unauthorized' })
     }
 
