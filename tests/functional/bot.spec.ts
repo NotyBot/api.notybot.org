361c1f3cab6a2668b6e797fdbde4ddee5f055c6d
Thibault <contactazarox@gmail.com>
2023-04-20 14:12:10 +0200

    test: implement test for bot controller

--- tests/functional/bot.spec.ts
+++ tests/functional/bot.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '@japa/runner'
 import Database from '@ioc:Adonis/Lucid/Database'
+import ApiCredential from 'App/Models/ApiCredential'
 
 test.group('Bot', (group) => {
   group.each.setup(async () => {
@@ -9,19 +10,45 @@ test.group('Bot', (group) => {
 
   test('should return api credential', async ({ client, assert }) => {
     const response = await client.post('/v1/api-credentials')
-
     assert.equal(response.status(), 200)
   })
 
   test('should return token', async ({ client, assert }) => {
+    const data = await ApiCredential.query().firstOrFail()
     const response = await client
       .post('/v1/bot')
       .json({
-        api_key: '82117a012388a81c05818834139b3ed5',
-        api_secret: '30fb9bc0674b0d5841ddc673a4f18a4b858cf507325efa20dcf8fb7bd308111d',
+        api_key: data.api_key,
+        api_secret: data.api_secret,
       })
       .send()
 
     assert.equal(response.status(), 200)
+  })
+
+  test('should return error when api key is invalid', async ({ client, assert }) => {
+    const data = await ApiCredential.query().firstOrFail()
+    const response = await client
+      .post('/v1/bot')
+      .json({
+        api_key: 'invalid',
+        api_secret: data.api_secret,
+      })
+      .send()
+
+    assert.equal(response.status(), 401)
+  })
+
+  test('should return error when api secret is invalid', async ({ client, assert }) => {
+    const data = await ApiCredential.query().firstOrFail()
+    const response = await client
+      .post('/v1/bot')
+      .json({
+        api_key: data.api_key,
+        api_secret: 'invalid',
+      })
+      .send()
+
+    assert.equal(response.status(), 401)
   })
 })
