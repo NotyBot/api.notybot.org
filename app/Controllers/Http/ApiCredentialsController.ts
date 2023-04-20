88cb6326d337157e5e62ab887abadbafd749c510
Thibault <contactazarox@gmail.com>
2023-04-20 14:20:47 +0200

    feat(bot): implement update for api credentials

--- app/Controllers/Http/ApiCredentialsController.ts
+++ app/Controllers/Http/ApiCredentialsController.ts
@@ -1,8 +1,25 @@
 import ApiCredential from 'App/Models/ApiCredential'
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import * as crypto from 'crypto'
 
 export default class ApiCredentialsController {
   public async store({}: HttpContextContract) {
     return await ApiCredential.create({})
+  }
+
+  public async update({ response, params }: HttpContextContract) {
+    const apiCredential = await ApiCredential.findBy('api_key', params.api_key)
+
+    if (!apiCredential) {
+      return response.status(401).json({ message: 'Api Key invalid' })
+    }
+
+    const apiSecret = await apiCredential
+      .merge({
+        api_secret: crypto.randomBytes(32).toString('hex'),
+      })
+      .save()
+
+    return response.send(apiSecret)
   }
 }
