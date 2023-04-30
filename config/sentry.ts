ba78b13bd22ac1d8171319377ee85572aaa836d6
Thibault <contactazarox@gmail.com>
2023-04-30 10:54:33 +0200

    reformat: sentry

--- config/sentry.ts
+++ config/sentry.ts
@@ -2,6 +2,6 @@ import { SentryConfig } from '@ioc:Adonis/Addons/Sentry'
 import Env from '@ioc:Adonis/Core/Env'
 
 export default {
-	dsn: Env.get('SENTRY_DSN'),
-	enabled: true,
+  dsn: Env.get('SENTRY_DSN'),
+  enabled: true,
 } as SentryConfig
