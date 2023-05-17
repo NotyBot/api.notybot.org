272569400ce5a986664bca36c7ea40835662d71f
Thibault <contactazarox@gmail.com>
2023-05-17 15:28:59 +0200

    feat: add index to welcome feature

--- app/Controllers/Http/Bot/WelcomesController.ts
+++ app/Controllers/Http/Bot/WelcomesController.ts
@@ -1,1 +1,7 @@
-export default class WelcomesController {}
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class WelcomesController {
+  public async create({ response }: HttpContextContract) {
+    return response.globalSuccess('Welcome to the bot')
+  }
+}
